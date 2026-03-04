105c5bceb388c1f8c8cf1795820a6e93da8ddd2d
Dewan Mehedi Hasan <tanjildewan10@gmail.com>
2026-03-04 22:41:59 +0600

    replace localhoast with production

--- ugc_songs_revenue/settings.py
+++ ugc_songs_revenue/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ty(e=+00-zdd807@-*9ynd$_e-slmh9qx1q2ee6ps@yo)3@lm%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost','revenue-backend-sp63.onrender.com']
 
